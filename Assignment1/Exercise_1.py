b83c4c0cf1b7d1d779dde060378c31920e1470a9
Zarak Mahmud <zarak.mahmud@gmail.com>
2012-09-26 15:10:05 -0400

    Uploaded class assignment 1 (From September 25th 20120)

--- Assignment1/Exercise_1.py
+++ Assignment1/Exercise_1.py
@@ -1,19 +1,12 @@
 #Assignment 1
 #Exercise 1
 
-#diet = ''
-#fruit = ['Banana', 'Cherry', 'Orange', 'Mango', 'Strawberry']
-#fruit.sort()
-#for i in fruit:
-    #diet += i + ' + '
-#print diet
-
 #Define the string to be inserted between each element of the list 'fruit'.
 space = ' + '
 #Populate the list fruit with some arbitrary fruits.
 fruit = ['Banana', 'Cherry', 'Orange', 'Mango', 'Strawberry']
-#This command takes elements from the list fruit and inserts the string
-#'space' between them.
+#This command combines the elements from the list fruit into the new string
+#'diet', and inserts the string 'space' between them.
 diet = space.join(fruit)
 #Print the result.
 print diet
