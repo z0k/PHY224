d88c1a61a00c73f7737caaa55f86ccbbdc16b6a9
z0k <zarak.mahmud@gmail.com>
2012-10-11 07:23:14 -0400

    Large angles in linear fit.

--- Classwork/linear_fitting.py
+++ Classwork/linear_fitting.py
@@ -4,7 +4,8 @@ from pylab import *
 from scipy.optimize import leastsq
 
 
-data = loadtxt('time_length.txt')
+data = loadtxt('time_length.txt', dtype='float')
+data2 = loadtxt('angle_time.txt', dtype='float')
 
 period = data[:,0]
 print period
@@ -12,28 +13,44 @@ print period
 length = data[:,1]
 print length
 
+angle = (pi / 180.) * data2[:,0]
+print angle
+
+period2 = data2[:,1]
+print period2
+
+
 g = 980.665
 p = zeros(2)
 p[0] = 0.
-p[1] = (4. * pi ** 2) / g
+p[1] = (4. * pi ** 2) / g 
 
 
-period_square = period ** 2
+period_squared = period ** 2
+period_squared_error = 2. * period * 0.05
+
+#Need to define theta max. 
+period_nonlinear = period[0] * (1. + 0.25 * sin(angle / 2.) ** 2)
+
 
 def peval(length, p):
     return p[1] * length + p[0]
 
 
-def residuals(p, period_square, length):
-    err = period_square - peval(length, p)
-    return err
+def residuals(p, period_squared, length):
+    return period_squared - peval(length, p)
 
-plsq = leastsq(residuals, p, args=(period_square, length), maxfev=2000)
 
+plsq = leastsq(residuals, p, args=(period_squared, length), maxfev=2000)
+
+title('No Idea Whats Going On')
 xlabel('Length')
 ylabel('Period$^2$')
-errorbar(length, period_square, 0.05, fmt='r+')
-plot(length, period_square, marker='o', linestyle='None')
+errorbar(length, period_squared, period_squared_error, fmt='r+')
+plot(length, period_squared, marker='o', linestyle='None')
 plot(length, peval(length, p))
+show()
 
+title('Experimental Data')
+plot(sin(angle / 2.) ** 2, period2)
 show()
